ccf150498dbe43896f923c3515152d3b2249b4df
machseven-studio <machseven.studio@gmail.com>
2026-09-01 19:11:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+HTML_PATH = os.path.join(BASE_DIR, "index.html")
+
 @app.get("/", response_class=HTMLResponse)
 def read_root():
-    if os.path.exists("index.html"):
-        with open("index.html", "r", encoding="utf-8") as f:
+    if os.path.exists(HTML_PATH):
+        with open(HTML_PATH, "r", encoding="utf-8") as f:
             return f.read()
-    return "<h1>index.html file not found!</h1>"
+    return "<h1>index.html file not found! Check your folder root.</h1>"
 
 if __name__ == "__main__":
     import uvicorn
